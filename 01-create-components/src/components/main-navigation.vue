287e8d8d02e81693d9f1bf3e728aa2522f649236
Deme Zalán <deme.zalan.janos@diak.szbi-pg.hu>
2026-09-23 12:56:28 +0200

    asd

---
+++ 01-create-components/src/components/main-navigation.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li><a href="/">Vue app</a></li>
+            <li><a href="https://vuejs.org">Vue docs</a></li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+    nav {
+        background-color: #333;
+        color: #fff;
+        padding: 1rem;
+    }
+
+    ul {
+        list-style: none;
+        display: flex;
+        gap: 1rem;
+    }
+
+    a {
+        color: #fff;
+        text-decoration: none;
+    }
+</style>
